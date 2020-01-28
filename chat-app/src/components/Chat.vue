c499fffc961483baa4131ebb5eba701324c31439
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-01-28 20:22:49 +0200

    Ready to be deployed. Will add more features

--- chat-app/src/components/Chat.vue
+++ chat-app/src/components/Chat.vue
@@ -3,7 +3,7 @@
     <h2 class="center teal-text">Private Chat</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
+        <ul class="messages" v-chat-scroll>
           <li v-for="message in messages" :key="message.id">
             <span class="teal-text">{{ message.name }}</span>
             <span class="grey-text text-darken-3">{{ message.content }}</span>
@@ -11,7 +11,8 @@
           </li>
         </ul>
       </div>
-
+      <hr>
+      
       <NewMessage :name="name" />
 
     </div>
@@ -20,6 +21,7 @@
 
 <script>
 import db from '@/firebase/init'
+import moment from 'moment'
 import NewMessage from '@/components/NewMessage'
 
 export default {
@@ -34,7 +36,7 @@ export default {
     }
   },
   created(){
-    let ref = db.collection('messages')
+    let ref = db.collection('messages').orderBy('timestamp')
 
     ref.onSnapshot(snapshot => {
       snapshot.docChanges().forEach( change => {
@@ -44,7 +46,7 @@ export default {
             id: doc.id,
             name:doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('LLL')
           })
         }
       })
@@ -66,6 +68,22 @@ export default {
 
 .chat .time{
   display: block;
-  font-size: 1.2em;
+  font-size: 0.8em;
 }
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}
+.messages::-webkit-scrollbar{
+  width: 3px;
+}
+.messages::-webkit-scrollbar-track{
+  background: #ddd;
+}
+
+.messages::-webkit-scrollbar-thumb{
+  background: #aaa;
+}
+
+
 </style>
