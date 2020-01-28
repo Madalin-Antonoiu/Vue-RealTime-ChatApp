c9d002cde86c11b138f146b13abfa51fff7965ae
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-01-28 05:14:01 +0000

    Welcome Form Finished.

---
+++ chat-app/src/components/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chat container">
+    <h2>Chat {{ this.name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: ['name', 'room'],
+  data(){
+    return {
+      name: "name"
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
--- chat-app/src/components/Welcome.vue
+++ chat-app/src/components/Welcome.vue
@@ -3,11 +3,27 @@
     <div class="card">
       <div class="card-content center-align">
         <h2 class="teal-text">Welcome</h2>
-          <form @submit.prevent="enterChat">
-            <label for="name">Enter your name</label>
-              <input type="text" name="name" v-model="name">
-              <button class="btn teal">Enter Chat</button>
+          <form @submit.prevent="validateAndEnterChat">
+
+        <div class="input-field">
+          <i class="material-icons prefix">assistant</i>
+          <input id="room" name="room" type="text"  class="validate" v-model="room" :class="{ 'invalid': attemptSubmit && missingRoom, 'valid': !missingRoom }">
+          <label for="room">Room Name</label>
+          <span  class="helper-text" data-error="You must enter room name to join." data-success="Correct"></span> <!-- Not writting anything inside for a helper -->
+        </div>
+
+        <div class="input-field">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="name" name="name" type="text"  class="validate" v-model="name" :class="{ 'invalid': attemptSubmit && missingName , 'valid': !missingName }">
+          <label for="name">Your Name</label>
+          <span class="helper-text" data-error="You must enter your name to join." data-success="Correct"></span>
+        </div>
+
+
+
+            <button class="btn teal">Enter Chat</button>
           </form>
+
       </div>
     </div>
   </div>
@@ -18,13 +34,37 @@ export default {
   name: 'Welcome',
   data () {
     return {
-      name: null
+      room: "",
+      name: "",
+      attemptSubmit: false,
     }
   },
+  computed: {
+    missingName() {  return  this.name === '' }, ////document.getElementById('room').classList.add('invalid');
+    missingRoom() {  return  this.room === '' },
+  },
   methods : {
-    enterChat(){
-      console.log(this.name);
+    validateAndEnterChat(){
+        this.attemptSubmit = true; // Attempted to submit
+        if (this.missingName){
+         document.getElementById('name').classList.add('invalid');
+
+        }
+        else if ( this.missingRoom) {
+            document.getElementById('room').classList.add('invalid');
+          //event.preventDefault(); // Prevent the click if one of those is wrong
+        } else {
+          this.$router.push({ name: 'Chat', params: {name: this.name, room: this.room} }) // Get the data and
+        }
+
+
+
+
     }
+
+
+
+
   }
 }
 </script>
@@ -37,9 +77,15 @@ export default {
  }
  .welcome h2 {
   font-size: 3em;
+  margin-bottom: 50px;
  }
  .welcome button {
   margin: 30px auto;
  }
-
+.hide{
+  display: none;
+}
+.welcome span{
+  margin-bottom: 30px;
+}
 </style>
