99507ed135e1c4e3083e1d084110ddf7f786fed6
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-01-31 16:31:06 +0200

    Revert "Preparing a Node Express Server for Sockets"
    This reverts commit a49c0548d7fb62f60b20dd022d97c12968f9e4e8.

--- chat-app/madaserver.js
+++
@@ -1,9 +0,0 @@
-//Initial configuration
-var express = require('express');
-var app = express();
-var server = app.listen(3000);
-
-//Hosting my entire chat app
-app.use(express.static('madapublic'));
-
-console.log('Triggered my server');
